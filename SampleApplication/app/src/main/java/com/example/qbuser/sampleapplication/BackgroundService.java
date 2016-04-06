3be1c3c0e0f3afcc4ef7aa4e37bb6a9fb12b4bb9
Sethuraj <sethuraj@qburst.com>
2016-04-06 15:50:12 +0530

    commit2

--- SampleApplication/app/src/main/java/com/example/qbuser/sampleapplication/BackgroundService.java
+++ SampleApplication/app/src/main/java/com/example/qbuser/sampleapplication/BackgroundService.java
@@ -1,11 +1,16 @@
 package com.example.qbuser.sampleapplication;
 
+import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -52,24 +57,63 @@ public class BackgroundService extends Service
         super.onDestroy();
     }
 
-
+    //Function to start the updating task
     private void startService()
     {
-        timer.scheduleAtFixedRate(new mainTask(), 0, 500);
-
-        Log.d(TAG,"call to start maintask recieved");
+        //Call the updating task every 500ms
+        timer.scheduleAtFixedRate(new UpdatingTask(), 0, 500);
+        //Log here
+        Log.d(TAG,"call to start UpdatingTask received");
     }
 
-    private class mainTask extends TimerTask
+
+    //The updating task class
+    private class UpdatingTask extends TimerTask
     {
         public void run()
         {
             Log.d(TAG,"Task is running");
+            //toastHandler.sendEmptyMessage(0);
         }
     }
 
 
+    private final Handler toastHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            String activityOnTop;
+
+            Log.d(TAG,"My function is getting called");
+
+            //Get an instance of activity manager
+            ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            //Get a list of running tasks
+            List<ActivityManager.RunningTaskInfo> RunningTask = mActivityManager.getRunningTasks(1);
+
+            ActivityManager.RunningTaskInfo ar = RunningTask.get(0);
+            activityOnTop=ar.topActivity.getClassName();
+
+            if(activityOnTop.equals("com.whatsapp.Conversation"))
+            {
+                pActivity = activityOnTop.toString();
+            }
+            else {
+                if (activityOnTop.equals(pActivity) || activityOnTop.equals("com.whatsapp.Conversation")) {
+
+                } else {
+                    Intent i = new Intent(BackgroundService.this, MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    Toast.makeText(BackgroundService.this, pActivity, 1).show();
+                    pActivity = activityOnTop.toString();
+
+                }
+            }
+        }
 
 
+    };
 
 }
